363207e3425db089943b59830b5499401e2804be
Harvar3105 <jupetr3105@outlook.com>
2026-04-26 21:39:33 +0300

    fix

--- src/utils/toQueryString.ts
+++ src/utils/toQueryString.ts
@@ -4,12 +4,11 @@ export function toQueryString(obj: object, prefix = ""): URLSearchParams {
   Object.entries(obj).forEach(([key, value]) => {
     if (value === null || value === undefined || value === "") return;
 
-    const fullKey = prefix ? `${prefix}.${key}` : key;
 
     if (typeof value === "object" && !Array.isArray(value)) {
-      toQueryString(value, fullKey).forEach((v, k) => params.append(k, v));
+      toQueryString(value, key).forEach((v, k) => params.append(k, v));
     } else {
-      params.append(fullKey, String(value));
+      params.append(key, String(value));
     }
   });
 
